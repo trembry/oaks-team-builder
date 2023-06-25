d4bb733b56f2ef5895908be0b53a8d3d1405ea6f
ttrammell98 <ttrammell98@ksu.edu>
2023-06-25 13:03:53 -0700

    Adding Basic Navigation

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,13 +1,29 @@
 "use client"
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import { Tabs, TabList, Tab } from '@chakra-ui/react'
+import { useRouter } from 'next/navigation'
 
 export default function NavBar() {
+    const router = useRouter();
+
+    const handleTabChange = (index: Number) => {
+        if (index === 0) 
+            router.push("/");
+        if (index === 1)
+            router.push("/types");
+        if (index === 2)
+            router.push("/team");
+        if (index === 3) {
+            router.push("/pokemon");
+        }
+    }
+
     return (
-        <Tabs isFitted variant='unstyled'>
-            <TabList>
-                <Tab _selected={{ color: 'white', bg: 'blue.500' }}>Home</Tab>
-                <Tab _selected={{ color: 'white', bg: 'green.400' }}>Types</Tab>
-                <Tab _selected={{ color: 'white', bg: 'red.400' }}>Pokemon</Tab>
+        <Tabs isFitted variant='unstyled' onChange={(index) => handleTabChange(index)}>
+            <TabList height={100}>
+                <Tab _selected={{ borderBottomWidth: '4px', borderBottomColor: 'blue.500', color: 'black', bg: 'gray.100' }}>Home</Tab>
+                <Tab _selected={{ borderBottomWidth: '4px', borderBottomColor: 'green.400', color: 'black', bg: 'gray.100' }}>Types</Tab>
+                <Tab _selected={{ borderBottomWidth: '4px', borderBottomColor: 'red.400', color: 'black', bg: 'gray.100' }}>Team</Tab>
+                <Tab _selected={{ borderBottomWidth: '4px', borderBottomColor: 'yellow.300', color: 'black', bg: 'gray.100' }}>Pokemon</Tab>    
             </TabList>
         </Tabs>
     )
---
+++ app/pokemon/page.tsx
@@ -0,0 +1,6 @@
+export default function PokemonPage() {
+    return (
+      <h2>Pokemon page coming soon...</h2>
+    )
+  }
+  
